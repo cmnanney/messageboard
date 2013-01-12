62110806f078dbd2f9863e19abbcf07923078f42
Christina Nanney <cmnanney@gmail.com>
2013-01-12 11:49:25 -0600

    New app messageboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messageboard::Application.config.secret_token = 'c436004ed7c311b01f9ac2f62231cc4f9503620f2a42b99f4c8ffe588efb7bd0801f3a17604eeca993f3085bda7d603a80b95aa5609025f9ff0e9ef13d1d9975'
